6b42ea923de1c58741ec0a98b1486c1a0804a88d
canercalskan <cvn3r8@gmail.com>
2022-07-29 15:53:02 +0300

    make admin navbar responsive, design prod details page

--- src/app/components/pages/products/details/product-details.component.ts
+++ src/app/components/pages/products/details/product-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from "@angular/router";
 import { ItemsService } from "src/app/services/admin/items.service";
 import { OnInit } from "@angular/core";
 import { Item } from "src/app/models/item.model";
+import { UserService } from "src/app/services/user/user.service";
 
 @Component({
     selector : 'product-details',
@@ -11,12 +12,27 @@ import { Item } from "src/app/models/item.model";
 })
 export class ProductDetails implements OnInit {
     product! : Item;
-    constructor(private route : ActivatedRoute , private ItemService : ItemsService){}
+    deliveryShown : boolean = false;
+    constructor(private route : ActivatedRoute , private ItemService : ItemsService , private UserService : UserService){}
     ngOnInit() {
         this.route.params.subscribe(params => { 
             this.ItemService.getProduct(params["productKey"]).subscribe( product => {
                this.product = product;
             })
         })
+    }
+    showDelivery() : void {
+        let message = document.getElementById('delivery-hidden');
+        message!.style.display = 'block';
+        this.deliveryShown = true;
+    }
+    hideDelivery():void {
+        let message = document.getElementById('delivery-hidden');
+        message!.style.display = 'none';
+        this.deliveryShown = false;
+    }
+
+    addToCart(item:Item) : void {
+        this.UserService.addToCart(item);
     }
 }
--- src/app/layout/navbar/admin/admin-navbar.component.ts
+++ src/app/layout/navbar/admin/admin-navbar.component.ts
@@ -7,12 +7,24 @@ import { Router } from "@angular/router";
     styleUrls : ['./admin-navbar.component.css']
 })
 
-export class AdminNavbar{
+export class AdminNavbar {
     constructor(private fireAuth : AngularFireAuth , private router : Router){}
+    navbarVisible : boolean = false;
     signOut():void {
         localStorage.removeItem('admin')
         this.fireAuth.signOut();
         this.router.navigate(['AdminPaneLogin'])
-        
+    }
+
+    showNavbar(): void  {
+        let navbar = document.getElementById('navbarNav');
+        navbar!.style.display = 'block'
+        this.navbarVisible = true;
+    }
+
+    hideNavbar() : void {
+        let navbar = document.getElementById('navbarNav');
+        navbar!.style.display = 'none';
+        this.navbarVisible = false;
     }
 }
